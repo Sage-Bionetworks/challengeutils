fc94335539dcc0dd74e31ac081d6608b12e28eee
Thomas Yu <thomasyu888@gmail.com>
2017-07-06 13:49:18 -0700

    Create DREAM challenge to team mapping table

--- statistics/createChallengeStatDf.py
+++ statistics/createChallengeStatDf.py
@@ -28,44 +28,23 @@ def createChallengeLocationList(allMembers,locationFileName):
 		for i in locations:
 			location_doc.write(i.encode('utf-8') + "\n")
 
-#Get team locations
-#Need to get participants before 2013
-challenges= {"Whole-Cell Parameter Estimation":[2223721],#2013
-			 "NIEHS-NCATS-UNC Toxicogenetics":[2223722,2223723], #2013
-			 "HPN-DREAM Breast Cancer Network Inference":[2223724,2223725,2223726,2223728],#2013
-			 "Rheumatoid Arthritis Responder":[2223744],#2014
-			 "SMC-DNA":[2223743],#2014
-			 "Alzheimer's Disease Big Data":[2223741],#2014
-			 "Acute Myeloid Leukemia Outcome Prediction":[3320951],#2014
-			 "Broad-DREAM Gene Essentiality Prediction":[3320895],#2014
-			 "SMC-Het":[3328713], #2015
-			 "DREAM Olfaction Prediction":[3323870], #2015
-			 "Prostate Cancer":[3325264], #2015
-			 "ALS Stratification Prize4Life":[3328255], #2015
-			 "AstraZeneca-Sanger Drug Combination Prediction":[3329051], #2015
-			 "SMC-DNA Meta":[3328707],#2015
-			 "Respiratory Viral":[3332517],#2016
-			 "Disease Module Identification":[3342189],#2016
-			 "ENCODE":[3340988], #2016
-			 "SMC-RNA":[3323365],#2016
-			 "DREAM Idea Challenge":[3341755],#2016
-			 "Digital Mammography":[3342365], #2016
-			 "Multiple Myeloma":[3342899],#2017 preregister
-			 "NCI-CPTAC Proteogenomics":[3351110], #2017 preregister
-			 "Parkinsons Disease Digital Biomarker":[3354062] #2017 preregister
-			}
+#EDIT syn10163902 TO ADD CHALLENGES
+challengesTable = syn.tableQuery('select * from syn10163902')
+challengesTableDf = challengesTable.asDataFrame()
 
 teams = []
 createdOn = []
 allUsers = []
 allLocations = []
-for chal in challenges:
+for chal in challengesTableDf['challenge']:
 	print(chal)
-	teams.append(','.join([str(i) for i in challenges[chal]]))
-	team = syn.getTeam(challenges[chal][0])
+	chalTeams = challengesTableDf['teams'][challengesTableDf['challenge'] == chal].values[0]
+	teams.append(chalTeams)
+	allTeams =chalTeams.split(",")
+	team = syn.getTeam(allTeams[0])
 	chal_users = set()
 	chal_loc = []
-	for i in challenges[chal]:
+	for i in allTeams:
 		for member in syn.getTeamMembers(i):
 			chal_users.add(member['member']['userName'])
 			member = syn.getUserProfile(member['member']['ownerId'])
@@ -77,7 +56,7 @@ for chal in challenges:
 	allLocations.append("|".join(chal_loc))
 
 challenge_data = pd.DataFrame()
-challenge_data['challenges'] = challenges.keys()
+challenge_data['challenges'] = challengesTableDf['challenge']
 challenge_data['teams'] = teams
 challenge_data['createdOn'] = createdOn
 #Some of the dates are wrong
@@ -92,16 +71,4 @@ challenge_data['createdOn'][challenge_data['challenges'] == "Respiratory Viral"]
 
 challenge_data['users'] = allUsers
 challenge_data['locations'] = allLocations
-
 challenge_data.to_csv("challenge_stats.tsv",index=False, sep='\t',encoding='utf-8')
-
-
-
-# for index,i in enumerate(challenge_data['challenges']):
-# 	i = i.replace('DREAM','')
-# 	i = i.replace('Challenge','')
-# 	i = i.replace('challenge','')
-# 	i = i.replace('1','')
-# 	i = i.replace('  ',' ')
-# 	print(i)
-# 	challenge_data['challenges'][index] = i
