776403c6741f2835143fca8d1986cc425f46a22e
thomasyu888 <thomasyu888@gmail.com>
2018-10-17 00:33:24 -0700

    Add in setup of dream utils package

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(name='dreamutils',
+      version='0.0.1',
+      description='DREAM utility functions',
+      url='https://github.com/Sage-Bionetworks/DREAM-Utilities',
+      author='Thomas Yu',
+      author_email='thomasyu888@gmail.com',
+      license='MIT',
+      packages=find_packages(),
+      zip_safe=False,
+      entry_points = {
+        'console_scripts': ['dreamutils = dreamutils.__main__:main']},
+      install_requires=[
+        'pandas>=0.20.0',
+        'synapseclient'])
