ac253bf6a993e24f3b40eb7509ec350af813447b
andrewelamb <andrewelamb@gmail.com>
2019-03-06 13:21:06 -0800

    script does not need evaluation queue as argument

--- challengeutils/download_current_lead_submission.py
+++ challengeutils/download_current_lead_submission.py
@@ -5,13 +5,17 @@ import os
 
 
 def get_submitterid_from_submission_id(submissionid, queue, synapse_object):
-    query = "select * from " + queue + " where objectId == " + str(submissionid)
-    generator = challengeutils.utils.evaluation_queue_query(synapse_object, query)
+    query = ("select * from " + queue + 
+             " where objectId == " + str(submissionid))
+    generator = challengeutils.utils.evaluation_queue_query(
+            synapse_object, query)
     lst = list(generator)
     if len(lst) == 0:
         raise Exception('submission id {} not in queue'.format(submissionid))
     submission_dict = lst[0]
     submitterid = submission_dict['submitterId']
+    if args.verbose:
+        print("submitterid: " + submitterid)
     return(submitterid)
 
 def get_submitters_lead_submission(submitterid, queue, synapse_object):
@@ -35,18 +39,44 @@ def get_submitters_lead_submission(submitterid, queue, synapse_object):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument("-i", "--submissionid", required=True, help="Int, or str(int) for submissionid, of current submission.")
-    parser.add_argument("-q", "--queue", required=True, help="string of evaluation queue, such as evaluation_1")
-    parser.add_argument("-c", "--synapse_config", required=True, help="credentials file")
-    parser.add_argument("-s", "--status", required=True, help="Submission status")
-    parser.add_argument("-v", "--verbose", action='store_false')
-    parser.add_argument("-a", "--cutoff_annotation", default = "met_cutoff")
+    parser.add_argument(
+            "-i", 
+            "--submissionid", 
+            required=True, 
+            help="Int, or str(int) for submissionid, of current submission.")
+    
+    parser.add_argument(
+            "-c",
+            "--synapse_config", 
+            required=True,
+            help="credentials file")
+    
+    parser.add_argument(
+            "-s",
+            "--status", 
+            required=True, 
+            help="Submission status")
+    
+    parser.add_argument(
+            "-v",
+            "--verbose",
+            action='store_false')
+    
+    parser.add_argument(
+            "-a", 
+            "--cutoff_annotation",
+            default = "met_cutoff")
+    
     args = parser.parse_args()
     
     if args.status == "VALIDATED":
         syn = synapseclient.Synapse(configPath=args.synapse_config)
         syn.login()
-        submitterid = get_submitterid_from_submission_id(args.submissionid, args.queue, syn)
-        path = get_submitters_lead_submission(submitterid, args.queue, syn)
+        current_sub = syn.getSubmissionStatus(args.submissionid)
+        queue_num = current_sub.annotations['scopeId']
+        queue = "evaluation_" + queue_num
+        submitterid = get_submitterid_from_submission_id(
+                args.submissionid, queue, syn)
+        path = get_submitters_lead_submission(submitterid, queue, syn)
     
     
