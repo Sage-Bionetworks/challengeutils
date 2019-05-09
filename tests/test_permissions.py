dfbd9b620b104179f1b2b3db34c301d21b6ad599
thomasyu888 <thomasyu888@gmail.com>
2019-05-08 20:31:02 -0700

    Lint and reconfigure unit tests

--- tests/test_permissions.py
+++ tests/test_permissions.py
@@ -1,40 +1,56 @@
-from mock import patch, call
+from mock import patch
 import pytest
 import challengeutils
 import synapseclient
-
-def setup_function(function):
-    print('\nsetup_function()')
-
-
-def test_wrong_permission_level():
-    with pytest.raises(ValueError, match=r'permission_level must be one of these:.*'):
-        challengeutils.permissions.set_entity_permissions("syn", "entity", principalid=None, permission_level="foo")
+syn = synapseclient.Synapse()
+SET_PERMS = {"set"}
 
 
-@pytest.fixture( params=[
+@pytest.fixture(params=[
     # tuple with (input, expectedOutput)
-    ("syn", None, None, "view"),
-    ("syn", None, None, "download"),
-    ("syn", None, None, "edit"),
-    ("syn", None, None, "edit_and_delete"),
-    ("syn", None, None, "admin"),
-    ("syn", None, None, "delete"),
+    (synapseclient.Project(), None, "view",
+        challengeutils.permissions.ENTITY_PERMS_MAPPINGS['view']),
+    (synapseclient.Folder(parentId="syn123"), None, "download",
+        challengeutils.permissions.ENTITY_PERMS_MAPPINGS['download']),
+    (synapseclient.Entity(), None, "edit",
+        challengeutils.permissions.ENTITY_PERMS_MAPPINGS['edit']),
+    (synapseclient.Schema(parentId="syn123"), None, "edit_and_delete",
+        challengeutils.permissions.ENTITY_PERMS_MAPPINGS['edit_and_delete']),
+    (synapseclient.File(parentId="syn123"), None, "admin",
+        challengeutils.permissions.ENTITY_PERMS_MAPPINGS['admin']),
+    (synapseclient.Entity(), None, "remove",
+        challengeutils.permissions.ENTITY_PERMS_MAPPINGS['remove']),
+    (synapseclient.Evaluation(contentSource="syn123"), None, "view",
+        challengeutils.permissions.EVALUATION_PERMS_MAPPINGS['view']),
+    (synapseclient.Evaluation(contentSource="syn123"), None, "submit",
+        challengeutils.permissions.EVALUATION_PERMS_MAPPINGS['submit']),
+    (synapseclient.Evaluation(contentSource="syn123"), None, "score",
+        challengeutils.permissions.EVALUATION_PERMS_MAPPINGS['score']),
+    (synapseclient.Evaluation(contentSource="syn123"), None, "admin",
+        challengeutils.permissions.EVALUATION_PERMS_MAPPINGS['admin']),
+    (synapseclient.Evaluation(contentSource="syn123"), None, "remove",
+        challengeutils.permissions.EVALUATION_PERMS_MAPPINGS['remove']),
     ])
 def test_data(request):
     return request.param
 
-def test_none_principalid(test_data):
-    (syn, entity, principalid, permission_level) = test_data
-    with pytest.raises(ValueError, match='principalid must not be None'):
-        challengeutils.permissions.set_entity_permissions(syn, entity, principalid=principalid, permission_level=permission_level)
+
+def test__set_permissions(test_data):
+    (entity, principalid, permission_level, mapped) = test_data
+    with patch.object(syn, "setPermissions",
+                      return_value=SET_PERMS) as patch_syn_set_permission:
+        challengeutils.permissions._set_permissions(
+            syn, entity, principalid=principalid,
+            permission_level=permission_level)
+        patch_syn_set_permission.assert_called_once_with(
+            entity,
+            principalId=principalid,
+            accessType=mapped)
 
 
-def test_set_permissions():
-    syn = synapseclient.Synapse()
-    permissions = {"set"}
-    with patch.object(syn, "setPermissions", return_value=permissions) as patch_syn_set_permission:
-        challengeutils.permissions.set_entity_permissions(syn, "syn12345", principalid="12345", permission_level="view")
-        patch_syn_set_permission.assert_called_once_with("syn12345",accessType=['READ'],principalId='12345')
-
-
+def test_wrong_permission_level():
+    with pytest.raises(ValueError,
+                       match=r'permission_level must be one of these:.*'):
+        challengeutils.permissions._set_permissions(
+            syn, synapseclient.Entity(), principalid="3",
+            permission_level="foo")
