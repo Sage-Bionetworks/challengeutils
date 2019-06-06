de1400e08bd5b49b62aced3464160af83cc70d8e
thomasyu888 <thomasyu888@gmail.com>
2019-06-05 22:36:41 -0700

    Abstract validation function

--- scoring_harness/challenge.py
+++ scoring_harness/challenge.py
@@ -61,6 +61,62 @@ def import_config_py(config_path):
     return(module)
 
 
+def validate_single_submission(syn, submission, status,
+                               validation_func, goldstandard_path,
+                               dry_run=False):
+    '''
+    Validates a single submission
+
+    Args:
+        syn: Synapse object
+        submission: Submission object
+        status: Submission Status object
+        validation_func: Function that validates (takes prediction filepath and
+                         truth file)
+        dry_run: Defaults to storing status (False)
+
+    Return:
+        is_valid - Boolean value for whether submission is valid
+        validation_error - Type of Python error (Assertion, ValueError...)
+        validation_message - Error message
+    '''
+    validation_error = None
+    logging.info("validating {} {}".format(submission.id, submission.name))
+    try:
+        # Account for if submissions aren't files
+        if submission.filePath is None:
+            submission_input = submission
+        else:
+            submission_input = submission.filePath
+        is_valid, validation_message = \
+            validation_func(submission_input, goldstandard_path)
+    except Exception as ex1:
+        is_valid = False
+        logging.error(
+            "Exception during validation: {} {} {}".format(
+                type(ex1), ex1, str(ex1)))
+        # ex1 only happens in this scope in python3,
+        # so must store validation_error as a variable
+        validation_error = ex1
+        validation_message = str(ex1)
+
+    status.status = "VALIDATED" if is_valid else "INVALID"
+
+    if not is_valid:
+        failure_reason = {"FAILURE_REASON": validation_message[0:1000]}
+    else:
+        failure_reason = {"FAILURE_REASON": ''}
+
+    add_annotations = to_submission_status_annotations(
+        failure_reason, is_private=False)
+    status = challengeutils.utils.update_single_submission_status(
+        status, add_annotations)
+
+    if not dry_run:
+        status = syn.store(status)
+    return(is_valid, validation_error, validation_message)
+
+
 def validate(syn,
              queue_info_dict,
              admin_user_ids,
@@ -69,6 +125,22 @@ def validate(syn,
              send_messages=False,
              acknowledge_receipt=False,
              dry_run=False):
+    '''
+    Validates all submissions with status = 'RECEIVED' by default and
+    emails participants with validation results
+
+    Args:
+        syn: Synapse object
+        queue_info_dict: dictionary with id, scoring_func,
+                         and goldstandard_path as keys
+        admin_user_ids: list of Synapse user profile ids of admin users
+        challenge_synid: Synapse id of challenge project
+        status: submissions with this status to validate. Default to
+                RECEIVED
+        send_messages: Send messages
+        acknowledge_receipt: Send validation results
+        dry_run: Do not update Synapse
+    '''
     evaluation = queue_info_dict['id']
     validation_func = queue_info_dict['validation_func']
     goldstandard_path = queue_info_dict['goldstandard_path']
@@ -86,41 +158,12 @@ def validate(syn,
         # to be later replaced by a "downloadFiles" flag on
         # getSubmissionBundles
         submission = syn.getSubmission(submission)
-        # Must define validation_error in case there is no error
-        validation_error = None
-        logging.info("validating {} {}".format(submission.id, submission.name))
-        try:
-            # Account for if submissions aren't files
-            if submission.filePath is None:
-                submission_input = submission
-            else:
-                submission_input = submission.filePath
-            is_valid, validation_message = \
-                validation_func(submission_input, goldstandard_path)
-        except Exception as ex1:
-            is_valid = False
-            logging.error(
-                "Exception during validation: {} {} {}".format(
-                    type(ex1), ex1, str(ex1)))
-            # ex1 only happens in this scope in python3,
-            # so must store validation_error as a variable
-            validation_error = ex1
-            validation_message = str(ex1)
 
-        status.status = "VALIDATED" if is_valid else "INVALID"
-
-        if not is_valid:
-            failure_reason = {"FAILURE_REASON": validation_message[0:1000]}
-        else:
-            failure_reason = {"FAILURE_REASON": ''}
-
-        add_annotations = to_submission_status_annotations(
-            failure_reason, is_private=False)
-        status = challengeutils.utils.update_single_submission_status(
-            status, add_annotations)
-
-        if not dry_run:
-            status = syn.store(status)
+        is_valid, validation_error, validation_message = \
+            validate_single_submission(
+                syn, submission, status,
+                validation_func, goldstandard_path,
+                dry_run=dry_run)
         # send message AFTER storing status to ensure
         # we don't get repeat messages
         profile = syn.getUserProfile(submission.userId)
@@ -229,7 +272,6 @@ def score(syn,
         send_messages: Send messages
         send_notifications: Send notifications
         dry_run: Do not update Synapse
-
     '''
     evaluation = queue_info_dict['id']
     scoring_func = queue_info_dict['scoring_func']
