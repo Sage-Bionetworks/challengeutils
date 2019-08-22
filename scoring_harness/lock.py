43b0531b55f75522b77e76659f78332f8c23d870
thomasyu888 <thomasyu888@gmail.com>
2019-08-22 00:40:59 -0700

    Comment out argparse for lock

--- scoring_harness/lock.py
+++ scoring_harness/lock.py
@@ -1,6 +1,9 @@
-import argparse
+'''
+Creates and tracks a lock file
+'''
+# import argparse
 import errno
-import inspect
+# import inspect
 import os
 import shutil
 import sys
@@ -11,38 +14,49 @@ LOCK_DEFAULT_MAX_AGE = timedelta(hours=2)
 
 
 class LockedException(Exception):
+    '''
+    Exception raised when scoring harness is already running
+    '''
     pass
 
 
 def acquire_lock_or_fail(name, max_age=LOCK_DEFAULT_MAX_AGE):
+    '''
+    Acquire lock file or determine that lock file exists
+
+    Args:
+        name: Name of lock file
+        max_age: The amount of time the lock file will live
+    '''
     lock = Lock(name, max_age=max_age)
     if lock.acquire():
         return lock
-    raise LockedException("A lock exists named {} who's age is: {}".format(
-        name, lock.get_age()))
+    raise LockedException(f"A lock exists named {name} who's age is: {lock.get_age()}")
 
 
-class Lock(object):
+class Lock:
     """
     Implements a lock by making a directory named [lockname].lock
     """
     SUFFIX = 'lock'
 
-    def __init__(self, name, dir=None, max_age=LOCK_DEFAULT_MAX_AGE):
+    def __init__(self, name, directory=None, max_age=LOCK_DEFAULT_MAX_AGE):
         self.name = name
         self.held = False
-        self.dir = dir if dir else os.path.dirname(os.path.abspath(__file__))
-        self.lock_dir_path = os.path.join(
-            self.dir, ".".join([name, Lock.SUFFIX]))
+        self.directory = directory if directory else os.path.dirname(os.path.abspath(__file__))
+        self.lock_dir_path = os.path.join(self.directory,
+                                          ".".join([name, Lock.SUFFIX]))
         self.max_age = max_age
 
     def get_age(self):
-        age = timedelta(
-            seconds=time.time() - os.path.getmtime(self.lock_dir_path))
+        '''
+        Get amount of time lock file has existed in seconds
+        '''
+        age = timedelta(seconds=time.time() - os.path.getmtime(self.lock_dir_path))
         return age
 
     def acquire(self, break_old_locks=True):
-        """Try to acquire lock. Return True on success or False otherwise"""
+        '''Try to acquire lock. Return True on success or False otherwise'''
         try:
             os.makedirs(self.lock_dir_path)
             self.held = True
@@ -75,33 +89,37 @@ class Lock(object):
                     raise
 
 
-def _sleep(seconds=0):
-    print("sleeping", seconds, "seconds")
-    for i in range(seconds):
-        time.sleep(1)
-        sys.stdout.write('.')
-        sys.stdout.flush()
-    print("\ndone sleeping")
+# def _sleep(seconds=0):
+#     print("sleeping", seconds, "seconds")
+#     for _ in range(seconds):
+#         time.sleep(1)
+#         sys.stdout.write('.')
+#         sys.stdout.flush()
+#     print("\ndone sleeping")
 
 
-if __name__ == "__main__":
-    lock = acquire_lock_or_fail('foo', max_age=timedelta(seconds=10))
-    try:
-        parser = argparse.ArgumentParser()
+# def main():
+#     lock = acquire_lock_or_fail('foo', max_age=timedelta(seconds=10))
+#     try:
+#         parser = argparse.ArgumentParser()
 
-        subparsers = parser.add_subparsers(title="subcommand")
+#         subparsers = parser.add_subparsers(title="subcommand")
 
-        parser_sleep = subparsers.add_parser('sleep')
-        parser_sleep.add_argument("seconds", type=int, default=0)
-        parser_sleep.set_defaults(func=_sleep)
+#         parser_sleep = subparsers.add_parser('sleep')
+#         parser_sleep.add_argument("seconds", type=int, default=0)
+#         parser_sleep.set_defaults(func=_sleep)
 
-        args = parser.parse_args()
+#         args = parser.parse_args()
 
-        # get a subset of the dictionary containing just the arguments of func
-        arg_spec = inspect.getargspec(args.func)
-        args_for_func = {k: getattr(args, k) for k in arg_spec.args}
+#         # get a subset of the dictionary containing just the arguments of func
+#         arg_spec = inspect.getargspec(args.func)
 
-        args.func(**args_for_func)
+#         args_for_func = {k: getattr(args, k) for k in arg_spec.args}
 
-    finally:
-        lock.release()
+#         args.func(**args_for_func)
+
+#     finally:
+#         lock.release()
+
+# if __name__ == "__main__":
+#     main()
