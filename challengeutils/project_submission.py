f9d7ffe4b2cc8459fd9c6ff177c954ab3b955c66
thomasyu888 <thomasyu888@gmail.com>
2019-09-21 10:17:01 -0700

    Update variable names and refactor

--- challengeutils/project_submission.py
+++ challengeutils/project_submission.py
@@ -8,13 +8,14 @@ import time
 import pandas as pd
 import synapseclient
 from synapseclient.annotations import to_submission_status_annotations
+from synapseclient.utils import id_of
 import synapseutils
 from . import utils
 logging.basicConfig(level=logging.INFO)
 LOGGER = logging.getLogger(__name__)
 
 
-# def _create_copy_project(syn, entityid, project_name):
+# def create_copy_project(syn, entityid, project_name):
 #     """
 #     Create a copy of a project
 
@@ -29,47 +30,52 @@ LOGGER = logging.getLogger(__name__)
 #     return entity
 
 
-def _create_archive_project_submission(syn, sub):
+def _create_archive_project_submission(syn, submission):
     """
     Creates the archived writeup project
 
     Args:
-        syn: Synapse object
-        sub: Synapse submission
+        syn: `synapseclient.Synapse` connection
+        submission: `synapseclient.Submission` object
 
     Returns:
-        Synapse Project entity
+        Archived `synapseclient.Project` object
     """
-    submission_name = sub.entity.name
+    submission_name = submission.entity.name
     current_time_ms = int(round(time.time() * 1000))
     archived_name = (f"Archived {submission_name} {current_time_ms} "
-                     f"{sub.id} {sub.entityId}")
+                     f"{submission.id} {submission.entityId}")
     #entity = _create_copy_project(syn, sub.entityId, archived_name)
-    project_entity = synapseclient.Project(archived_name)
-    entity = syn.store(project_entity)
-    synapseutils.copy(syn, sub.entityId, entity.id)
-    return entity
+    archive_project = synapseclient.Project(archived_name)
+    archive_project_entity = syn.store(archive_project)
+    synapseutils.copy(syn, submission.entityId, archive_project_entity.id)
+    return archive_project_entity
 
 
-def archive_project_submission(syn, submissionid, rearchive=False):
+def archive_project_submission(syn, submission, rearchive=False):
     """
     Archive one writeup submission by copying the submitted writeup
     Project into a new Project.
 
     Args:
-        syn: Synapse object
-        submissionid: Synapse submission objectId
+        syn: `synapseclient.Synapse` connection
+        submission: `synapseclient.Submission` or its id
         rearchive: Determine whether or not to re-archive the submitted
                    Project. Default to False.
+
+    Returns:
+        Synapse entity id of archived project
     """
     # retrieve file into cache and copy it to destination
-    sub = syn.getSubmission(submissionid, downloadFile=False)
-    sub_status = syn.getSubmissionStatus(submissionid)
+    if not isinstance(submission, synapseclient.Submission):
+        sub = syn.getSubmission(submission, downloadFile=False)
+    sub_status = syn.getSubmissionStatus(submission)
     # The .get accounts for if there is no stringAnnos
     archived_entity = [x for x in sub_status.annotations.get('stringAnnos', [])
                        if x.get("key") == "archived"]
     # archived_entity will be an empty list if the annotation doesnt exist
     if not archived_entity or rearchive:
+        LOGGER.info(f"Archiving project submission: {sub.id}")
         entity = _create_archive_project_submission(syn, sub)
         archived = {"archived": entity.id}
         sub_status = utils.update_single_submission_status(sub_status,
@@ -79,7 +85,9 @@ def archive_project_submission(syn, submissionid, rearchive=False):
     return archived_entity[0]['value']
 
 
-def archive_project_submissions(syn, evaluation, status="VALIDATED",
+def archive_project_submissions(syn, evaluation,
+                                status_key="STATUS",
+                                status="VALIDATED",
                                 rearchive=False):
     """
     Archive submissions for an evaluation queue that accepts writeup
@@ -87,54 +95,26 @@ def archive_project_submissions(syn, evaluation, status="VALIDATED",
     folder.
 
     Args:
-        syn: Synapse object
-        evaluation: a synapse evaluation queue or its ID
+        syn: `synapseclient.Synapse` connection
+        evaluation: `synapseclient.Evaluation` or its id
         status: Annotation status of a submission. Defaults to VALIDATED
         rearchive: Determine whether or not to re-archive the submitted
                    Project. Default to False.
+
     Returns:
         List of archived entity ids
     """
-    if not isinstance(evaluation, synapseclient.Evaluation):
-        evaluation = syn.getEvaluation(evaluation)
+    evaluationid = id_of(evaluation)
+    LOGGER.info(f"Archiving queue: {evaluationid}")
+    query = (f"select objectId from evaluation_{evaluationid}"
+             f"where {status_key} == {status}")
+    query_results = utils.evaluation_queue_query(syn, query)
 
-    LOGGER.info(f"Archiving {evaluation.id} {evaluation.name}")
-    LOGGER.info("-" * 60)
-    archived = [archive_project_submission(syn, sub.id, rearchive=rearchive)
-                for sub, _ in syn.getSubmissionBundles(evaluation,
-                                                       status=status)]
+    archived = [archive_project_submission(syn, query_result['objectId'],
+                                           rearchive=rearchive)
+                for query_result in query_results]
     return archived
 
-# This function already exists to an extent. annotate_submission_with_json
-def add_annotations_to_submission(syn, submissionid, row, keys):
-    """
-    Attach the write up synapse id and archived write up synapse id on
-    the main submission
-
-    Args:
-        syn: synapse object
-        submissionid: submission id
-        row: pd.Series or dict of submission annotations to add
-        keys: list of keys to add to the submission
-    """
-    status = syn.getSubmissionStatus(submissionid)
-    # Filter for only the keys to add to submission
-    # Don't add if the value is null
-    add_annotation_dict = {key: row[key] for key in keys
-                           if pd.isnull(row[key])}
-    # If archive hasnt been run, there won't be an archive
-    add_annotations = to_submission_status_annotations(add_annotation_dict,
-                                                       is_private=False)
-    new_status = utils.update_single_submission_status(status,
-                                                       add_annotations)
-    syn.store(new_status)
-
-# def _filter_joined_leaderboard(subs_and_writeupsdf):
-#     validated = subs_and_writeupsdf[f'{status_key}_y'] == "VALIDATED"
-#     subs_and_writeupsdf = subs_and_writeupsdf[validated]
-#     scored = subs_and_writeupsdf[f'{status_key}_x'] == "SCORED"
-#     subs_and_writeupsdf = subs_and_writeupsdf[scored]
-#     return subs_and_writeupsdf
 
 def join_queues(syn, queue1, queue2, joinby, how="inner"):
     """
@@ -161,6 +141,14 @@ def join_queues(syn, queue1, queue2, joinby, how="inner"):
     return joineddf
 
 
+# def _filter_joined_leaderboard(subs_and_writeupsdf):
+#     validated = subs_and_writeupsdf[f'{status_key}_y'] == "VALIDATED"
+#     subs_and_writeupsdf = subs_and_writeupsdf[validated]
+#     scored = subs_and_writeupsdf[f'{status_key}_x'] == "SCORED"
+#     subs_and_writeupsdf = subs_and_writeupsdf[scored]
+#     return subs_and_writeupsdf
+
+
 def archive_and_attach_project_submissions(syn, writeup_queueid,
                                            submission_queueid,
                                            status_key="STATUS"):
@@ -168,25 +156,22 @@ def archive_and_attach_project_submissions(syn, writeup_queueid,
     Attach the write up to the submission queue
 
     Args:
-        syn: Synapse object
+        syn: `synapseclient.Synapse` connection
         writeup_queueid: Write up evaluation queue id
         submission_queueid: Submission queue id
         status_key: Submission status annotation key to look query.
                     Defaults to STATUS,
                     but could be prediction_file_status (workflowhook)
     """
-    # archive_project_submissions(syn, writeup_queueid, status="VALIDATED",
-    #                             rearchive=False)
+    archive_project_submissions(syn, writeup_queueid,
+                                status_key=status_key,
+                                status="VALIDATED",
+                                rearchive=False)
 
     subs_and_writeupsdf = join_queues(syn, submission_queueid,
                                       writeup_queueid,
                                       joinby="submitterId",
                                       how="left")
-
-    # if pd.isnull(row['entityId']):
-    #     LOGGER.info(f"NO WRITEUP: {row['submitterId']}")
-    # else:
-    #     LOGGER.info(f"ADD WRITEUP: {row['submitterId']}")
 
     # Filter joined leaderboard
     # subs_and_writeupsdf = _filter_joined_leaderboard(subs_and_writeupsdf)
@@ -202,7 +187,7 @@ def archive_and_attach_project_submissions(syn, writeup_queueid,
     # One writeup can be linked to many submissions, but not the other way
     # around
     subs_and_writeupsdf.drop_duplicates('objectId_x', inplace=True)             
-    
+
     # Must rename writeup submission objectId or there will be conflict
     # entityId_y: writeUp Project ids
     # archived: archived Project ids
@@ -216,19 +201,21 @@ def archive_and_attach_project_submissions(syn, writeup_queueid,
 
     annotation_keys = ['writeUp', 'archivedWriteUp']
 
+    # None value is added because annotate_submission replies on an `is None`
+    # check
     for key in annotation_keys:
-        # Add NA column if the column doesn't exist
+        # Add column with None values if the column doesn't exist
         if subs_and_writeupsdf.get(key) is None:
-            subs_and_writeupsdf[key] = float('nan')
-        # Replace all None values with float('nan')
+            subs_and_writeupsdf[key] = None
+        # Replace all float('nan') values with None
         else:
             null_ind = subs_and_writeupsdf[key].isnull()
-            subs_and_writeupsdf[key][null_ind] = float('nan')
+            subs_and_writeupsdf[key][null_ind] = None
 
     # objectId_x: objectIds from submission queue
     subs_and_writeupsdf.apply(lambda row:
-                              add_annotations_to_submission(syn,
-                                                            row['objectId_x'],
-                                                            row,
-                                                            annotation_keys),
+                              utils.annotate_submission(syn,
+                                                        row['objectId_x'],
+                                                        row[annotation_keys],
+                                                        annotation_keys),
                               axis=1)
