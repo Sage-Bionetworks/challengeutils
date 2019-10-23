c65fe3553af8dfbfae461f2a9e9b8d3dd0aced60
Thomas Yu <thomasyu888@gmail.com>
2019-10-23 16:02:10 -0700

    Add in validation of projects

--- templates/challenge_config.template.py
+++ templates/challenge_config.template.py
@@ -2,8 +2,9 @@
 from scoring_harness.queue_validator import EvaluationQueueValidator
 from scoring_harness.queue_scorer import EvaluationQueueScorer
 
-class Validate(EvaluationQueueValidator):
 
+class Validate(EvaluationQueueValidator):
+    """Example Validate interaction"""
     def interaction_func(self, submission, goldstandard_path):
         assert submission.filePath is not None, \
             "Submission must be a Synapse File and not Project/Folder"
@@ -16,8 +17,9 @@ class Validate(EvaluationQueueValidator):
                            'message': message}
         return submission_info
 
-class Score(EvaluationQueueScorer):
 
+class Score(EvaluationQueueScorer):
+    """Example Score interaction"""
     def interaction_func(self, submission, goldstandard_path):
         print(goldstandard_path)
         auc = 4
@@ -29,6 +31,63 @@ class Score(EvaluationQueueScorer):
                         'annotations': score_dict,
                         'message': message}
         return score_status
+
+
+class ValidateProject(EvaluationQueueScorer):
+    """Template for ValidateProject submissions"""
+    def interaction_func(self, submission, goldstandard_path,
+                         public=True, admin=None):
+        """Validates Projects
+
+        Args:
+            submission: Submission object
+            goldstandard_path: Unused
+            public: If the writeup needs to be public. Defaults to True
+            admin: Specify Synapse userid that writeup needs to be
+                   shared with
+        Returns:
+            validation status dict
+        """
+        from synapseclient import Submission, Project, AUTHENTICATED_USERS
+        from synapseclient.exceptions import SynapseHTTPError
+
+        not_writeup_error = (
+            "This is the writeup submission queue - submission must be a "
+            "Synapse Project.  Please submit to the subchallenge queues "
+            "for prediction file submissions."
+        )
+        assert isinstance(submission, Submission), not_writeup_error
+        assert isinstance(submission['entity'], Project), not_writeup_error
+        # Replace with the challenge project id here
+        assert submission.entityId != "syn1234", \
+            "Writeup submission must be your project and not the challenge site"
+
+        # Add in users to share this with
+        share_with = []
+        try:
+            if public:
+                message = f"Please make your private project ({submission['entityId']}) public"
+                share_with.append(message)
+                ent = self.syn.getPermissions(submission['entityId'],
+                                              AUTHENTICATED_USERS)
+                assert "READ" in ent and "DOWNLOAD" in ent, message
+                ent = self.syn.getPermissions(submission['entityId'])
+                assert "READ" in ent, message
+            if admin is not None:
+                message = (f"Please share your private directory ({submission['entityId']})"
+                           f" with the Synapse user `{admin}` with `Can Download` permissions.")
+                share_with.append(message)
+                ent = self.syn.getPermissions(submission['entityId'], admin)
+                assert "READ" in ent and "DOWNLOAD" in ent, message
+        except SynapseHTTPError as syn_error:
+            if syn_error.response.status_code == 403:
+                raise AssertionError("\n".join(share_with))
+            raise syn_error
+        validation_status = {'valid': True,
+                             'annotations': {},
+                             'message': "Validated!"}
+        return validation_status
+
 
 EVALUATION_QUEUES_CONFIG = [
     {
