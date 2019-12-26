47d0cbeae27f5eacae428c53ab4dc9872e73416e
thomasyu888 <thomasyu888@gmail.com>
2019-12-26 15:34:17 -0800

    Add test

--- tests/test_download_current_lead_submission.py
+++ tests/test_download_current_lead_submission.py
@@ -61,7 +61,7 @@ def test_get_submitters_lead_submission():
 
 
 def test_none_get_submitters_lead_submission():
-    """Tests getting of submitter id"""
+    """Tests not getting submission"""
     with patch.object(utils, "evaluation_queue_query",
                       return_value=[]) as patch_query:
         dl_file = dl_cur.get_submitters_lead_submission(SYN,
@@ -71,3 +71,33 @@ def test_none_get_submitters_lead_submission():
                                                         verbose=False)
         patch_query.assert_called_once()
         assert dl_file is None
+
+
+def test_download_current_lead_sub():
+    """Tests download of lead submission"""
+    submission = synapseclient.Submission(evaluationId='2', entityId='2',
+                                          versionNumber='3')
+
+
+    with patch.object(SYN, "getSubmission",
+                     return_value=submission) as patch_getsub,\
+        patch.object(dl_cur, "get_submitterid_from_submission_id",
+                     return_value="2") as patch_getsubmitter,\
+        patch.object(dl_cur, "get_submitters_lead_submission",
+                     return_value="path") as patch_get_lead:
+        dl_file = dl_cur.download_current_lead_sub(SYN,
+                                                   SUBMISSIONID,
+                                                   "VALIDATED",
+                                                   "key",
+                                                   verbose=False)
+        patch_getsubmitter.assert_called_once()
+        patch_get_lead.assert_called_once()
+        assert dl_file == "path"
+
+
+def test_invalid_download_current_lead_sub():
+    """Tests None is downloaded if status is INVALID"""
+    dl_file = dl_cur.download_current_lead_sub(SYN, SUBMISSIONID,
+                                               "INVALID", "key",
+                                               verbose=False)
+    assert dl_file is None
