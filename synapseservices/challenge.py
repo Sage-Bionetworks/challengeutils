acf65240ab3e878ed24bd4690a14980f06fceaab
thomasyu888 <thomasyu888@gmail.com>
2020-03-01 22:46:10 -0800

    add uri into challenge class

--- synapseservices/challenge.py
+++ synapseservices/challenge.py
@@ -1,3 +1,4 @@
+"""Challenge Services"""
 from dataclasses import dataclass
 
 
@@ -7,11 +8,33 @@ class Challenge:
 
     Args:
         id: The ID of this Challenge object
-        etag: Synapse employs an Optimistic Concurrency Control (OCC) scheme to handle concurrent updates.
+        etag: Synapse employs an Optimistic Concurrency Control (OCC)
+              scheme to handle concurrent updates.
         projectId: The ID of the Project the challenge is used with.
-        participantTeamId: The ID of the Team which users join to participate in the Challenge
+        participantTeamId: The ID of the Team which users join to participate
+                           in the Challenge
     """
     id: str = None
     projectId: str = None
     etag: str = None
     participantTeamId: str = None
+
+    def post_uri(self):
+        """Creates a challenge"""
+        return '/challenge'
+
+    def get_uri(self):
+        """Gets a challenge"""
+        return f'/challenge/{self.id}'
+
+    def get_from_project_uri(self):
+        """Gets a challenge from project"""
+        return f'/entity/{self.projectId}/challenge'
+
+    def put_uri(self):
+        """Updates a challenge"""
+        return f'/challenge/{self.id}'
+
+    def delete_uri(self):
+        """Deletes a challenge"""
+        return f'/challenge/{self.id}'
