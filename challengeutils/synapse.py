052b7a3563c87a4522fe4751cbc6e47b4c203e40
thomasyu888 <thomasyu888@gmail.com>
2020-03-17 23:01:42 -0700

    Add synapse connection object

---
+++ challengeutils/synapse.py
@@ -0,0 +1,42 @@
+"""
+Manage Synapse connection
+"""
+import logging
+
+import synapseclient
+from synapseclient.exceptions import SynapseAuthenticationError
+
+logging.basicConfig(level=logging.INFO)
+LOGGER = logging.getLogger(__name__)
+
+
+class Synapse:
+    """Define Synapse class"""
+    _synapse_client = None
+
+    @classmethod
+    def client(cls, syn_user=None, syn_pass=None, *args, **kwargs):
+        """Gets a logged in instance of the synapseclient.
+
+        Args:
+            syn_user: Synapse username
+            syn_pass: Synpase password
+
+        Returns:
+            logged in synapse client
+        """
+        if not cls._synapse_client:
+            LOGGER.debug("Getting a new Synapse client.")
+            cls._synapse_client = synapseclient.Synapse(*args, **kwargs)
+            try:
+                cls._synapse_client.login(silent=True)
+            except SynapseAuthenticationError:
+                cls._synapse_client.login(syn_user, syn_pass, silent=True)
+
+        LOGGER.debug("Already have a Synapse client, returning it.")
+        return cls._synapse_client
+
+    @classmethod
+    def reset(cls):
+        """Change synapse connection"""
+        cls._synapse_client = None
