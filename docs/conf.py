065b19c945e73661158728e3f719a9d9c77262b9
Thomas Yu <thomasyu888@gmail.com>
2020-05-16 14:48:30 -0700

    Update version number

--- docs/conf.py
+++ docs/conf.py
@@ -25,7 +25,7 @@ copyright = '2017-2020, Sage Bionetworks'
 author = 'Thomas Yu'
 
 # The short X.Y version
-version = '2.0'
+version = '2.1'
 # The full version, including alpha/beta/rc tags
 release = version
 
