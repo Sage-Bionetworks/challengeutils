dd88d069efc71d67f6e37e625936358c4cacde4e
Thomas Yu <thomas.yu@sagebase.org>
2020-06-13 17:04:32 -0700

    Updating annotations (#180)
    * Test annotations
    
    * Add in support to update a submission's status in annotation submission
    
    * Fix tess
    
    * Add tests for converting dict or synapse style annotations to Annotations cls
    
    * Test updating annotations
    
    * Add EOL
    
    * Add line
    
    * Fix

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -238,32 +238,6 @@ def test_specifyloc_download_submission():
         assert sub_dict == expected_submission_dict
 
 
-def test_annotate_submission_with_json():
-    add_annotations = {'test': 2, 'test2': 2}
-    tempfile_path = tempfile.NamedTemporaryFile()
-    with open(tempfile_path.name, "w") as annotation_file:
-        json.dump(add_annotations, annotation_file)
-    status = {"foo": "bar"}
-    with mock.patch.object(
-            syn, "getSubmissionStatus",
-            return_value=status) as patch_get_submission, \
-        mock.patch.object(
-            challengeutils.utils, "update_single_submission_status",
-            return_value=status) as patch_update, \
-            mock.patch.object(syn, "store") as patch_syn_store:
-        response = challengeutils.utils.annotate_submission_with_json(
-            syn, "1234", tempfile_path.name,
-            is_private=True,
-            force=False)
-        patch_get_submission.assert_called_once_with("1234")
-        patch_update.assert_called_once_with(
-            status, add_annotations,
-            is_private=True,
-            force=False)
-        patch_syn_store.assert_called_once_with(status)
-        assert response.status_code == 200
-
-
 def test_userid__get_submitter_name():
     """Get username if userid is passed in"""
     submitterid = 2222
