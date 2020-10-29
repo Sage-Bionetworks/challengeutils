218f1fbc1908f06be5a6b48e9900002469ad95a4
thomasyu888 <thomas.yu@sagebase.org>
2020-10-28 21:17:53 -0700

    Add tests

--- challengeutils/teams.py
+++ challengeutils/teams.py
@@ -1,24 +1,27 @@
 """Team related functions"""
+from typing import Union
+
+from synapseclient import Synapse, Team
 from synapseclient.core.utils import id_of
 
 
-def _get_team_count(syn, teamid):
-    """Get number of team members
+def _get_team_count(syn: Synapse, teamid: int) -> dict:
+    """Rest call wrapper for getting team member count
 
     Args:
         syn: Synapse object
-        team: Synapse team id
+        teamid: Synapse team id
     """
     count = syn.restGET(f"/teamMembers/count/{teamid}")
     return count
 
 
-def get_team_count(syn, team):
+def get_team_count(syn: Synapse, team: Union[int, str, Team]) -> int:
     """Get number of team members
 
     Args:
         syn: Synapse object
-        team: synaspeclient.Team or its id
+        team: synaspeclient.Team, its id, or name.
     """
     team_obj = syn.getTeam(team)
     count = _get_team_count(syn, team_obj.id)
