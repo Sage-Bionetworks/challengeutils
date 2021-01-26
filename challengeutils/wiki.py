fcdd6062e715074be30832f5006b458f8301ed75
thomasyu888 <thomas.yu@sagebase.org>
2021-01-26 21:08:13 +0800

    Use workdir parameter for all wiki commands

--- challengeutils/wiki.py
+++ challengeutils/wiki.py
@@ -1,6 +1,7 @@
 """Download and sync wiki tools"""
 import json
 import os
+import typing
 
 import synapseclient
 from synapseclient import Synapse
@@ -30,20 +31,41 @@ def pull_wiki(syn: Synapse, project: str, workdir: str = "./") -> dict:
                         if letter.isalnum())
         # Home page title is always blank
         if title == '':
-            title = 'home'
-        # Don't add the ./ if markdown location isn't specified
-        markdown_path = os.path.join(workdir,
-                                     f"{title}.md")
+            title = '_homepage_'
+        markdown_path = os.path.join(workdir, f"{title}.md")
         with open(markdown_path, 'w') as md_file:
             md_file.write(wiki['markdown'])
         wiki_header['markdown_path'] = f"{title}.md"
-    config_path = os.path.join(workdir, "wiki_config.json")
-    with open(config_path, 'w') as config:
-        json.dump(wiki_headers, config, indent=4)
     return wiki_headers
 
 
-def _validate_config(wiki_config: dict):
+def read_wiki_config(workdir: str) -> typing.List[dict]:
+    """Read wiki config file from working directory
+
+    Args:
+        workdir: Working directory
+
+    Returns:
+        Wiki Configuration
+        [
+            {
+                "id": "111",
+                "title": "title",
+                "parentId": "33333",
+                "markdown_path": "markdown.md"
+            },
+            {...}
+        ]
+    """
+    config_path = os.path.join(workdir, "wiki_config.json")
+    if not os.path.exists(config_path):
+        raise ValueError("wiki_config.json must exist at specified workdir")
+    with open(config_path, "r") as config:
+        wiki_config = json.load(config)
+    return wiki_config
+
+
+def validate_config(workdir: str):
     """Validates wiki configuration
 
     Args:
@@ -51,56 +73,35 @@ def _validate_config(wiki_config: dict):
 
     Raises:
         ValueError:
-            `markdown_path` is specified but cannot be located
-            There are more than one `title` that is ''
-            `id` is not specified and `markdown_path`/`parentId`/`title`
-            is missing or `parentId` not one of the `id`s.
+            `markdown_path` is specified but cannot be located.
+            There are duplicated wiki ids.
+            `id` is not specified and (`markdown_path`/`parentId`/`title`
+            is missing or `parentId` not one of the `id`s.)
 
     """
+    wiki_config = read_wiki_config(workdir)
     ids = [wiki_header['id'] for wiki_header in wiki_config
            if wiki_header.get('id') is not None]
-    home_page_count = 0
+    if len(set(ids)) != len(ids):
+        raise ValueError("Must have unique ids.")
     for wiki_header in wiki_config:
         markdown_path = wiki_header.get('markdown_path')
         wikiid = wiki_header.get('id')
         parentid = wiki_header.get('parentId')
-        title = wiki_header.get('title')
+        title = wiki_header.get('title', '')
         # Markdown file must exist if specified
-        if markdown_path is not None and not os.path.exists(markdown_path):
+        if markdown_path is not None and not os.path.exists(
+                os.path.join(workdir, markdown_path)):
             raise ValueError(f"{markdown_path} does not exist")
-        # Cannot have more than one page without a title (Only the home page)
-        # Can have no title
-        if title == '':
-            home_page_count += 1
-        if home_page_count > 1:
-            raise ValueError("Cannot have more than one page without a "
-                             "`title`")
         if (wikiid is None and
             (markdown_path is None or parentid not in ids
-             or title is None)):
+             or title == '')):
             raise ValueError("If `id` is not specified, then `markdown_path` "
                              "and `parentId`, and `title` must be specified. "
+                             "`title` must also not be blank."
                              "`parentId` must be one of the "
                              "`id`s in the config.")
-
-
-def validate_config(config_path: str):
-    """Validates wiki configuration
-
-    Args:
-        config_path: Wiki configuration path
-
-    Raises:
-        ValueError:
-            `markdown_path` is specified but cannot be located
-            There are more than one `title` that is ''
-            `id` is not specified and `markdown_path`/`parentId`/`title`
-            is missing or `parentId` not one of the `id`s.
-
-    """
-    with open(config_path, "r") as config:
-        wiki_config = json.load(config)
-    _validate_config(wiki_config)
+    return wiki_config
 
 
 def push_wiki(syn: Synapse, projectid: str, workdir: str = "./") -> dict:
@@ -109,19 +110,15 @@ def push_wiki(syn: Synapse, projectid: str, workdir: str = "./") -> dict:
     Args:
         syn: Synapse connection
         project: synapseclient.Project or its id
-        workdir: Location to download markdown files and wiki_config.json
-                 into. Defaults to location of where code is being
+        workdir: Location to download markdown files and wiki_config.json.
+                 Defaults to location of where code is being
                  executed.
 
     Returns:
         Dict of wiki configuration
 
     """
-    config_path = os.path.join(workdir, "wiki_config.json")
-    with open(config_path, "r") as config:
-        wiki_config = json.load(config)
-
-    _validate_config(wiki_config)
+    wiki_config = validate_config(workdir)
     for wiki_header in wiki_config:
         # no markdown path, nothing to update
         markdown_path = wiki_header.get('markdown_path')
@@ -131,18 +128,21 @@ def push_wiki(syn: Synapse, projectid: str, workdir: str = "./") -> dict:
         markdown_path = os.path.join(workdir, markdown_path)
         with open(markdown_path, 'r') as md_f:
             markdown = md_f.read()
+        # Create new wiki page if id isn't specified
         if wiki_header.get('id') is not None:
             wiki = syn.getWiki(projectid, subpageId=wiki_header['id'])
             # Don't store if the wiki pages are the same
             if wiki.markdown == markdown:
                 print(f"no updates: {wiki_header['title']}")
                 continue
+            print(f"Wiki updated: {wiki_header['title']}")
         else:
             wiki = synapseclient.Wiki(owner=projectid,
                                       title=wiki_header['title'],
                                       parentWikiId=wiki_header['parentId'])
-        print(f"Wiki added/updated: {wiki_header['title']}")
+            print(f"Wiki added: {wiki_header['title']}")
         wiki.markdown = markdown
-        syn.store(wiki)
-
+        wiki = syn.store(wiki)
+        # If new wiki page is added, must add to wiki_config.json
+        wiki_header['id'] = wiki['id']
     return wiki_config
