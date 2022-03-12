c666120cbe03f143018d88bcc7f990d42ed8a9c1
thomasyu888 <thomas.yu@sagebase.org>
2022-03-12 14:50:24 +0800

    Add HACK comment

--- setup.py
+++ setup.py
@@ -7,7 +7,8 @@ about = {}
 here = os.path.abspath(os.path.dirname(__file__))
 with open(os.path.join(here, "challengeutils", "__version__.py")) as f:
     exec(f.read(), about)
-
+# HACK: this must be kept because __init__ imports the discussion
+# modules which import requests which has to be installed first.
 setup(
     version=about["__version__"]
 )
