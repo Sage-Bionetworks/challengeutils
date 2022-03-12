071c70e6f0d984bbbdcda013380ff264c0ba2575
thomasyu888 <thomas.yu@sagebase.org>
2022-03-12 14:48:54 +0800

    lint and fix versioning

--- challengeutils/__init__.py
+++ challengeutils/__init__.py
@@ -1,3 +1,5 @@
 from . import discussion
 from . import permissions
 from .__version__ import __version__
+
+__all__ = [discussion, permissions, __version__]
--- setup.py
+++ setup.py
@@ -1,4 +1,13 @@
 """Setup"""
+import os
 from setuptools import setup
 
-setup()
+# figure out the version
+about = {}
+here = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(here, "challengeutils", "__version__.py")) as f:
+    exec(f.read(), about)
+
+setup(
+    version=about["__version__"]
+)
