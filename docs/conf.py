12645f24bb7bbbaa3baaef4d3fede2c212ca0342
Verena Chung <9377970+vpchung@users.noreply.github.com>
2023-11-30 13:36:28 -0800

    Release v4.3.0 (#253)
    * remove support for 3.7, add support for 3.11+
    
    * update version, add changelog for v4.3
    
    * update ci
    
    * lint
    
    * update action versions to latest
    
    * fix test for 3.12

--- docs/conf.py
+++ docs/conf.py
@@ -25,7 +25,7 @@ copyright = "2017-2023, Sage Bionetworks"
 author = "Thomas Yu, Verena Chung, Tim Bergquist"
 
 # The short X.Y version
-version = "4.2.1"
+version = "4.3.0"
 # The full version, including alpha/beta/rc tags
 release = version
 
